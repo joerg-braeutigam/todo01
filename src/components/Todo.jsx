0f311edc3f03d08688d08b93a4d02f3038327650
Joerg Braeutigam <braeutigam.joerg@gmail.com>
2023-07-23 19:56:37 +0200

    003

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Todo = ({ description, done }) => {
+const Todo = ({ description, done, onChangeTodo, onDeleteTodo, index }) => {
 	return (
-		<div className="flex items-center justify-between p-2">
-			<h2 className="text-lg">
-				{description}: {done ? "Erledigt" : "Offen"}
-			</h2>
-			<button className="text-lg bg-gray-800 text-yellow-50 p-2">
-				Löschen
-			</button>
-		</div>
+		<>
+			<div className={done ? "bg-green-900" : "bg-red-900"}>
+				<div className="flex items-center justify-between p-2">
+					<h2
+						className="text-lg text-white"
+						onClick={() => {
+							onChangeTodo(index);
+						}}>
+						{description}
+					</h2>
+					<button
+						className="text-lg bg-gray-800 text-yellow-50 p-2"
+						onClick={() => {
+							onDeleteTodo(index);
+						}}>
+						Löschen
+					</button>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,20 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Todo from "./Todo";
 
-const todos = [
+const alltodos = [
 	{ description: "Einkaufen", done: true },
-	{ description: "Bier holen", done: false },
+	{ description: "Sport", done: false },
 	{ description: "React App erstellen", done: false }
 ];
 const Todolist = () => {
+	const [opencount, setOpencount] = useState(0);
+	const [todos, setTodos] = useState(alltodos);
+
+	const countOpen = () => {
+		const donetodos = todos.filter(item => {
+			return !item.done;
+		});
+		setOpencount(donetodos.length);
+	};
+
+	const changeTodo = index => {
+		const newTodos = [...todos];
+		if (newTodos[index].done) {
+			newTodos[index].done = false;
+		} else {
+			newTodos[index].done = true;
+		}
+		setTodos(newTodos);
+	};
+
+	const deleteTodo = index => {
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
+
+	useEffect(() => {
+		countOpen();
+	}, [todos]);
+
 	return (
-		<div className="shadow-sm hover:shadow-2xl">
+		<div className="shadow-sm hover:shadow-xl">
 			<div className="text-center bg-gray-600 text-white text-3xl py-4">
-				<h1>Todolist</h1>
+				<h1 className="text-2xl">Todolist</h1>
+				<h2 className="text-sm">Offene Todos: {opencount}</h2>
 			</div>
 			{todos.map((item, index) => {
 				return (
-					<Todo description={item.description} done={item.done} key={index} />
+					<Todo
+						description={item.description}
+						done={item.done}
+						key={index}
+						index={index}
+						onChangeTodo={changeTodo}
+						onDeleteTodo={deleteTodo}
+					/>
 				);
 			})}
 		</div>
