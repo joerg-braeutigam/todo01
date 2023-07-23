e675a1febf737cdacf1f357eae836491a21932b4
Joerg Braeutigam <braeutigam.joerg@gmail.com>
2023-07-23 20:43:43 +0200

    finish 01

--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,14 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Todo from "./Todo";
 
-const alltodos = [
-	{ description: "Einkaufen", done: true },
-	{ description: "Sport", done: false },
-	{ description: "React App erstellen", done: false }
-];
 const Todolist = () => {
 	const [opencount, setOpencount] = useState(0);
-	const [todos, setTodos] = useState(alltodos);
+	const [todos, setTodos] = useState(() => {
+		const items = localStorage.getItem("items");
+		return JSON.parse(items) || [];
+	});
+	const [textinput, setTextinput] = useState("");
+
+	const changeText = e => {
+		setTextinput(e.target.value);
+	};
+
+	const submit = e => {
+		e.preventDefault();
+		const newTodos = [...todos, { description: textinput, done: false }];
+		setTodos(newTodos);
+		setTextinput("");
+	};
 
 	const countOpen = () => {
 		const donetodos = todos.filter(item => {
@@ -35,6 +45,7 @@ const Todolist = () => {
 
 	useEffect(() => {
 		countOpen();
+		localStorage.setItem("items", JSON.stringify(todos));
 	}, [todos]);
 
 	return (
@@ -42,6 +53,23 @@ const Todolist = () => {
 			<div className="text-center bg-gray-600 text-white text-3xl py-4">
 				<h1 className="text-2xl">Todolist</h1>
 				<h2 className="text-sm">Offene Todos: {opencount}</h2>
+				<form action="#" className="grid grid-cols-3 py-2">
+					<input
+						type="text"
+						name=""
+						id=""
+						className="text-black p-2 col-span-2 text-sm"
+						placeholder="Neues Todo"
+						onChange={changeText}
+						value={textinput}
+					/>
+					<input
+						type="submit"
+						value="Add Todo"
+						className="text-sm col-span-1 bg-orange-700"
+						onClick={submit}
+					/>
+				</form>
 			</div>
 			{todos.map((item, index) => {
 				return (
